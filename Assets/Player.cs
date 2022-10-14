ef3f2d2fa697f4e889637856aa132db90927b362
Skinny-Spider <115791106+Skinny-Spider@users.noreply.github.com>
2022-10-14 18:56:40 +0200

    Grundbewegungen funktionieren

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private float speed = 4.0f;
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * 7 * Time.deltaTime;
+        }
     }
 }

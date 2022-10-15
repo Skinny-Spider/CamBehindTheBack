8a9ec82721d10412ab9fff14a467135051b62f96
Skinny-Spider <115791106+Skinny-Spider@users.noreply.github.com>
2022-10-15 20:20:31 +0200

    haupmenü fixed

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,16 +11,16 @@ public class Player : MonoBehaviour
 
     public float mouseSensitivity;
 
-    private float speed = 4.0f;
+    private float speed = 1000;
 
     float leftRightRot;
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
-    
+
     void OnCollisionStay()
     {
         isGrounded = true;
@@ -36,36 +36,34 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            Debug.Log("####### JUMP  ######");
-            
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-            
         }
-        
+
         leftRightRot = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity; ;
         transform.Rotate(new Vector3(0, leftRightRot));
-       
+
 
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            rb.velocity = transform.right * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            rb.velocity = -transform.right * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.back * speed * Time.deltaTime;
+            
+            rb.velocity = transform.forward * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
+            rb.velocity = -transform.forward * speed * Time.deltaTime;
         }
 
-        
+
     }
 }
